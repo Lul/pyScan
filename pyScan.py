92053260893a984eaacf64a4ccfdc9e605b51268
John <59487177+Lul@users.noreply.github.com>
2020-01-17 16:42:32 -0700

    Add files via upload

--- pyScan.py
+++ pyScan.py
@@ -42,17 +42,16 @@ def menuselect():
             popup2.mainloop()
         
         if x == 3:
-            n = 0
             popup3 = Toplevel()            
             pscroll3 = Scrollbar(popup3)
             pscroll3.pack(side=RIGHT, fill=Y)
             lbl3 = Label(popup3, text="Select an IP to scan")
             listbox3 = Listbox(popup3, selectmode=SINGLE, height=10, width=20)
+            n = 0
             for x in enumnetwork():                
                 listbox3.insert(n, x)
                 n+=1
             btn3 = Button(popup3, text="Submit", command=lambda : scanselectip(text3, listbox3))
-            #btn3 = Button(popup3, text="Submit", command=lambda : printtest(listbox3))
             text3 = Text(popup3, wrap=NONE, yscrollcommand=pscroll3.set, height=20, width=60)
             lbl3.pack()
             listbox3.pack()
@@ -61,7 +60,41 @@ def menuselect():
             popup3.mainloop()
         
         if x == 4:
-            print("Coming Soon")
+            popup4 = Toplevel()
+            lbl4 = Label(popup4, text="Select an IP and a script to use")
+            listbox4frame = Frame(popup4)
+            pscroll4 = Scrollbar(listbox4frame)
+            listbox4 = Listbox(listbox4frame, selectmode=SINGLE, exportselection=0, yscrollcommand=pscroll4.set, height=5, width=15)
+            pscroll4.pack(side=RIGHT, fill=Y)
+            pscroll4.configure(command=listbox4.yview)
+            listbox42frame = Frame(popup4)
+            pscroll42 = Scrollbar(listbox42frame)
+            listbox42 = Listbox(listbox42frame, selectmode=SINGLE, exportselection=0, yscrollcommand=pscroll42.set, height=10, width=30)
+            pscroll42.pack(side=RIGHT, fill=Y)
+            pscroll42.configure(command=listbox42.yview)
+            text4frame = Frame(popup4)
+            pscroll43 = Scrollbar(text4frame)
+            text4 = Text(text4frame, wrap=NONE, yscrollcommand=pscroll43.set, height=15, width=60)
+            pscroll43.pack(side=RIGHT, fill=Y)
+            pscroll43.configure(command=text4.yview)
+            n = 0
+            for x in enumnetwork():
+                listbox4.insert(n, x)
+                n+=1
+            n2 = 0
+            for y in findscripts():
+                listbox42.insert(n2, y)
+                n2+=1
+            btn4 = Button(popup4, text="Submit", command=lambda : scriptscan(listbox4, listbox42, text4))
+            lbl4.pack()
+            listbox4frame.pack()
+            listbox42frame.pack()
+            text4frame.pack()
+            listbox4.pack()
+            listbox42.pack()
+            text4.pack()
+            btn4.pack()
+            popup4.mainloop()
             
         if x == 5:
             window.destroy()
@@ -127,6 +160,23 @@ def scanselectip(textwidget, listbox):
     result = nm.csv().replace(";", " ")
     textwidget.insert("1.0", result) 
     
+def findscripts():
+    scriptlist = []
+    for root, dirs, files in os.walk('/usr/share/nmap/scripts/'):
+        for name in files:
+            scriptlist.append(name)
+    return sorted(scriptlist)    
+    
+def scriptscan(listbox1, listbox2, textwidget):
+    for x in listbox1.curselection():
+        lbsel1 = listbox1.get(x)
+    for y in listbox2.curselection():
+        lbsel2 = listbox2.get(y)
+    nm = nmap.PortScanner()
+    nm.scan(lbsel1, '1-65535', arguments='--script=%s' % lbsel2)
+    result = nm.csv().replace(";", " ")
+    textwidget.insert("1.0", result)    
+    
 def nmapcheck():
     try:
         import nmap
@@ -141,7 +191,6 @@ def buttoncheck():
         btn['state'] = DISABLED
         
 window = Tk()
-
 window.title("pyScan")
 window.geometry('470x270')
 
@@ -151,7 +200,6 @@ syscheck = StringVar()
 lbl = Label(window, text="Select an Action")
 lbl2 = Label(window, textvariable=constat)
 lbl3 = Label(window, textvariable=syscheck)
-
 lb = Listbox(window, selectmode=SINGLE, height=7, width=55)
 lb.insert(0, "Display current machine's IP")
 lb.insert(1, "Enumerate all IPs on local subnet")
@@ -159,7 +207,6 @@ lb.insert(2, "Scan ports & enumerate OS/services on entire local subnet")
 lb.insert(3, "Scan ports & enumerate OS/services on local device IP")
 lb.insert(4, "Scan local device IP with script")
 lb.insert(5, "Exit")
-
 btn = Button(window, text="Submit", command=menuselect, state=DISABLED)
 btn2 = Button(window, text="Verify Connection & nmap", command=buttoncheck)
 
@@ -169,8 +216,7 @@ if verifyconnection():
 else:
     lbl2.config(fg='red')
     constat.set("Not Connected")
-    
-if nmapcheck():
+    if nmapcheck():
     lbl3.config(fg='black')
     syscheck.set("Nmap Module Found")
     btn['state'] = NORMAL
