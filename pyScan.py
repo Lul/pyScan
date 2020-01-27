f5b4177f838d09bb45f19f939e7942a37ba7adc7
John <59487177+Lul@users.noreply.github.com>
2020-01-27 16:41:44 -0700

    Update pyScan.py

--- pyScan.py
+++ pyScan.py
@@ -21,82 +21,60 @@ def menuselect():
         if x == 1:
             n = 0
             popup1 = Toplevel()
+            listbox1frame = Frame(popup1)
             pscroll1 = Scrollbar(popup1)
             pscroll1.pack(side=RIGHT, fill=Y)
-            lis1 = Listbox(popup1, selectmode=SINGLE, yscrollcommand=pscroll1.set, height=10, width=20)
+            lis1 = Listbox(listbox1frame, selectmode=SINGLE, yscrollcommand=pscroll1.set, height=10, width=20)
             for x in enumnetwork():
                 lis1.insert(n, x)
                 n+=1
             lis1.pack()
+            listbox1frame.pack()
+            pscroll1.configure(command=lis1.yview)
             popup1.mainloop()   
             
         if x == 2:            
             popup2 = Toplevel()
             pscroll2 = Scrollbar(popup2)
             pscroll2.pack(side=RIGHT, fill=Y)
-            text2 = Text(popup2, wrap=NONE, yscrollcommand=pscroll2.set, height=40, width=100)
+            textbox2frame = Frame(popup2)
+            text2 = Text(textbox2frame, wrap=NONE, yscrollcommand=pscroll2.set, height=40, width=100)
+            pscroll2.configure(command=text2.yview)
             for x in enumnetwork():
                result = scanip(x)
                text2.insert("1.0", result)
             text2.pack(side="left")
+            textbox2frame.pack()
+            pscroll2.configure(command=text2.yview)
             popup2.mainloop()
         
         if x == 3:
-            popup3 = Toplevel()            
-            pscroll3 = Scrollbar(popup3)
-            pscroll3.pack(side=RIGHT, fill=Y)
+            popup3 = Toplevel()    
             lbl3 = Label(popup3, text="Select an IP to scan")
-            listbox3 = Listbox(popup3, selectmode=SINGLE, height=10, width=20)
+            listbox3frame = Frame(popup3)
+            textbox3frame = Frame(popup3)
+            pscroll3 = Scrollbar(listbox3frame)
+            pscroll32 = Scrollbar(textbox3frame)
+            pscroll3.pack(side=RIGHT, fill=Y)
+            pscroll32.pack(side=RIGHT, fill=Y)
+            listbox3 = Listbox(listbox3frame, selectmode=SINGLE, yscrollcommand=pscroll3.set, height=10, width=20)
+            text3 = Text(textbox3frame, wrap=NONE, yscrollcommand=pscroll32.set, height=20, width=60)
             n = 0
             for x in enumnetwork():                
                 listbox3.insert(n, x)
                 n+=1
             btn3 = Button(popup3, text="Submit", command=lambda : scanselectip(text3, listbox3))
-            text3 = Text(popup3, wrap=NONE, yscrollcommand=pscroll3.set, height=20, width=60)
+            pscroll3.configure(command=listbox3.yview)
+            pscroll32.configure(command=text3.yview)
             lbl3.pack()
+            listbox3frame.pack()
+            textbox3frame.pack()
             listbox3.pack()
-            btn3.pack()  
             text3.pack()
+            btn3.pack()
             popup3.mainloop()
-        
+                            
         if x == 4:
-            popup4 = Toplevel()
-            lbl4 = Label(popup4, text="Select an IP and a script to use")
-            listbox4frame = Frame(popup4)
-            pscroll4 = Scrollbar(listbox4frame)
-            listbox4 = Listbox(listbox4frame, selectmode=SINGLE, exportselection=0, yscrollcommand=pscroll4.set, height=5, width=15)
-            pscroll4.pack(side=RIGHT, fill=Y)
-            pscroll4.configure(command=listbox4.yview)
-            listbox42frame = Frame(popup4)
-            pscroll42 = Scrollbar(listbox42frame)
-            listbox42 = Listbox(listbox42frame, selectmode=SINGLE, exportselection=0, yscrollcommand=pscroll42.set, height=10, width=30)
-            pscroll42.pack(side=RIGHT, fill=Y)
-            pscroll42.configure(command=listbox42.yview)
-            text4frame = Frame(popup4)
-            pscroll43 = Scrollbar(text4frame)
-            text4 = Text(text4frame, wrap=NONE, yscrollcommand=pscroll43.set, height=15, width=60)
-            pscroll43.pack(side=RIGHT, fill=Y)
-            pscroll43.configure(command=text4.yview)
-            n = 0
-            for x in enumnetwork():
-                listbox4.insert(n, x)
-                n+=1
-            n2 = 0
-            for y in findscripts():
-                listbox42.insert(n2, y)
-                n2+=1
-            btn4 = Button(popup4, text="Submit", command=lambda : scriptscan(listbox4, listbox42, text4))
-            lbl4.pack()
-            listbox4frame.pack()
-            listbox42frame.pack()
-            text4frame.pack()
-            listbox4.pack()
-            listbox42.pack()
-            text4.pack()
-            btn4.pack()
-            popup4.mainloop()
-            
-        if x == 5:
             window.destroy()
             sys.exit(1)
             
@@ -192,7 +170,7 @@ def buttoncheck():
         
 window = Tk()
 window.title("pyScan")
-window.geometry('470x270')
+window.geometry('%sx%s' % (window.winfo_screenwidth(), window.winfo_screenheight()))
 
 constat = StringVar()
 syscheck = StringVar()
@@ -205,8 +183,7 @@ lb.insert(0, "Display current machine's IP")
 lb.insert(1, "Enumerate all IPs on local subnet")
 lb.insert(2, "Scan ports & enumerate OS/services on entire local subnet")
 lb.insert(3, "Scan ports & enumerate OS/services on local device IP")
-lb.insert(4, "Scan local device IP with script")
-lb.insert(5, "Exit")
+lb.insert(4, "Exit")
 btn = Button(window, text="Submit", command=menuselect, state=DISABLED)
 btn2 = Button(window, text="Verify Connection & nmap", command=buttoncheck)
 
